03bad0d16c9d15b96f404006261420d4fb2fabdb
joleye <zl@joleye.com>
2014-08-16 17:37:49 +0800

    增加了post方式

--- js/jquery.action.js
+++ js/jquery.action.js
@@ -11,13 +11,35 @@ $.fn.action = function(){
 		
 		var func = 'act_'+conf.act;
 		
-		act_func[func] && act_func[func](conf);
+		act_func[func] && act_func[func](conf,this);
 	});
 };
 
 var act_func = {
 	act_link : function(conf){
 		location.href = conf.url;
+	},
+	act_post : function(conf,dthis){
+		var ext = conf.extend?conf.extend:{};
+		
+		ye.verify(conf.form,ext).do_post({
+			method : 'ajax',
+			msg : {
+				right : 'dright',
+				error : 'derr'
+			},
+			btn : {
+				name : dthis,
+				text : 'load...'
+			},
+			success : function(env){
+				if(env.result){
+					$.messager.alert("系统提示",env.msg);
+				}else{
+					$.messager.alert("错误提示",env.msg);
+				}
+			}
+		});
 	}
 };
 
